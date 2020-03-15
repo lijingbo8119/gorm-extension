9ba94e948e75fb5429c103c40aac8de9fea1061a
lijingbo8119 <me@ljb.ink>
2020-03-15 12:14:56 +0800

    update

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/lijingbo8119/gorm-extension
 go 1.12
 
 require (
-	github.com/gogf/gf v1.9.7
-	github.com/jinzhu/gorm v1.9.10
+	github.com/gogf/gf v1.11.5
+	github.com/jinzhu/gorm v1.9.12
 )
--- scopes/sorter.go
+++ scopes/sorter.go
@@ -27,10 +27,15 @@ func Sorter(sorter map[string]interface{}, model interface{}) func(db *gorm.DB) 
 			case "id", "create_time", "update_time":
 				db = db.Order(k + " " + gconv.String(v))
 			default:
+				tagKey := k
+				if strings.Contains(k, "_formatted") {
+					tagKey = strings.Split(k, "_formatted")[0]
+				}
+
 				for i := 0; i < t.NumField(); i++ {
 					jsonTag := t.Field(i).Tag.Get("json")
-					if jsonTag == k {
-						db = db.Order(k + " " + gconv.String(v))
+					if jsonTag == tagKey {
+						db = db.Order(tagKey + " " + gconv.String(v))
 					}
 				}
 			}
